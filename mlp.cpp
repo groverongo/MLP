1fe8ba8deb7f5a2cdc5797060cf8d283a830e075
Sebastian Knell <sebastianknell@hotmail.com>
2023-06-02 11:59:42 -0500

    first commit

---
+++ mlp.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sebastian Knell on 2/06/23.
+//
+
+#include "mlp.h"
---
+++ mlp.h
@@ -0,0 +1,20 @@
+//
+// Created by Sebastian Knell on 2/06/23.
+//
+
+#ifndef MLP_MLP_H
+#define MLP_MLP_H
+
+struct Layer {
+
+};
+
+class MLP {
+    int n;
+    int hidden_size;
+public:
+    int forward();
+};
+
+
+#endif //MLP_MLP_H
