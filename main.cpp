ebe81cb72d155edc04d64be66713b8ac3a58cfd6
Prober76 <ugartegrover@gmail.com>
2023-06-08 13:24:54 -0500

    lo anterior

--- main.cpp
+++ main.cpp
@@ -5,34 +5,6 @@
 #include"cargar_csv.h"
 using namespace std;
 
-Eigen::MatrixXd readCSV(std::string file, int rows, int cols) {
-    std::ifstream in(file);
-    std::string line;
-
-    int row = 0;
-    int col = 0;
-
-    Eigen::MatrixXd res = Eigen::MatrixXd(rows, cols);
-    if (in.is_open()) {
-        while (std::getline(in, line)) {
-            char *ptr = (char *) line.c_str();
-            int len = line.length();
-            col = 0;
-            char *start = ptr;
-            for (int i = 0; i < len; i++) {
-                if (ptr[i] == ',') {
-                    res(row, col++) = atof(start);
-                    start = ptr + i + 1;
-                }
-            }
-            res(row, col) = atof(start);
-            row++;
-        }
-        in.close();
-    }
-    return res;
-}
-
 void prueba_XOR(){
     MatrixXd X(4, 2);
     X << 0,0, 1,0, 0,1, 1,1;
